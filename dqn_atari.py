0efc0ae19dce175906a8ba3bee9c376ed44403e9
Peiyun Hu <peiyunh.ph@gmail.com>
2017-03-24 01:03:16 -0400

    change default architecture and initializer

--- dqn_atari.py
+++ dqn_atari.py
@@ -44,88 +44,23 @@ def create_model_duel(window, input_shape, num_actions,
 
     input = Input(shape=(window, input_rows, input_cols))
 
-    l_1 = Conv2D(32, kernel_size=(8,8), strides=(4,4), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
-                     activation='relu', input_shape=(window,input_rows,input_cols))(input)
-
-    l_2 = Conv2D(64, kernel_size=(4,4), strides=(2,2), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
-                     activation='relu')(l_1)
-    l_3 = Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
-                     activation='relu')(l_2)
-
-    l_4 = Flatten()(l_3)
-
-    v_1 = Dense(256, activation='relu', kernel_initializer=initializer.RandomNormal(stdded=0.01))(l_4)
-    v_2 = Dense(1, kernel_initializer=initializer.RandomNormal(stdded=0.01))(v_1)
-    v_out = Lambda(lambda s: K.expand_dims(s[:, 0], axis=-1),
-                output_shape=(num_actions,))(v_2)
+    l_1 = Conv2D(16, kernel_size=(8,8), strides=(4,4), padding='same',
+                 kernel_initializer=initializers.he_normal(),
+                 activation='relu', input_shape=(window,input_rows,input_cols))(input)
+    l_2 = Conv2D(32, kernel_size=(4,4), strides=(2,2), padding='same',
+                 kernel_initializer=initializers.he_normal(),
+                 activation='relu')(l_1)
+    l_3 = Flatten()(l_2)
+    v_1 = Dense(128, activation='relu', kernel_initializer=initializers.he_normal())(l_3)
+    v_2 = Dense(1)(v_1)
+    v_out = Lambda(lambda s: K.expand_dims(s[:, 0], axis=-1), output_shape=(num_actions,))(v_2)
      
-    a_1 = Dense(256, activation='relu', kernel_initializer=initializer.RandomNormal(stdded=0.01))(l_4)
-    a_2 = Dense(num_actions, kernel_initializer=initializer.RandomNormal(stdded=0.01))(a_1)
-    a_out = Lambda(lambda a: a[:, :] - K.mean(a[:, :], keepdims=True), 
-        output_shape=(num_actions,))(a_2)
+    a_1 = Dense(128, activation='relu', kernel_initializer=initializers.he_normal())(l_3)
+    a_2 = Dense(num_actions)(a_1)
+    a_out = Lambda(lambda a: a[:, :] - K.mean(a[:, :], keepdims=True), output_shape=(num_actions,))(a_2)
 
-    # q_out = merge([v_out, a_out], mode='sum')
     q_out = keras.layers.merge([v_out, a_out], mode='add')
     model = Model(inputs=input, outputs=q_out)
-
-    return model
-
-def create_model_large(window, input_shape, num_actions,
-                       model_name='q_network'):  # noqa: D103
-    """Create the Q-network model.
-
-    Use Keras to construct a keras.models.Model instance (you can also
-    use the SequentialModel class).
-
-    We highly recommend that you use tf.name_scope as discussed in
-    class when creating the model and the layers. This will make it
-    far easier to understnad your network architecture if you are
-    logging with tensorboard.
-
-    Parameters
-    ----------
-    window: int
-      Each input to the network is a sequence of frames. This value
-      defines how many frames are in the sequence.
-    input_shape: tuple(int, int)
-      The expected input image size.
-    num_actions: int
-      Number of possible actions. Defined by the gym environment.
-    model_name: str
-      Useful when debugging. Makes the model show up nicer in tensorboard.
-
-    Returns
-    -------
-    keras.models.Model
-      The Q-model.
-    """
-    # Remember you messed up with the initializations
-
-    input_rows, input_cols = input_shape[0], input_shape[1]
-
-    print 'Now we start building the model ... '
-    model = Sequential() 
-    # model.add(Conv2D(16, kernel_size=(8,8), strides=(4,4), padding='same',
-    model.add(Conv2D(32, kernel_size=(8,8), strides=(4,4), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
-                     activation='relu', input_shape=(window,input_rows,input_cols)))
-    # model.add(Conv2D(32, kernel_size=(4,4), strides=(2,2), padding='same',
-    model.add(Conv2D(64, kernel_size=(4,4), strides=(2,2), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
-                     activation='relu'))
-    model.add(Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
-                     activation='relu'))
-    model.add(Flatten())
-    # model.add(Dense(256, activation='relu'))
-    model.add(Dense(512, activation='relu'))
-    model.add(Dense(num_actions, activation='linear')) 
-
-    # plot the architecture of convnet 
-    # plot_model(model, to_file='convnet.png')
 
     return model
 
@@ -166,64 +101,19 @@ def create_model_small(window, input_shape, num_actions,
     print 'Now we start building the model ... '
     model = Sequential() 
     model.add(Conv2D(16, kernel_size=(8,8), strides=(4,4), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
+                     kernel_initializer=initializers.he_normal(),
                      activation='relu', input_shape=(window,input_rows,input_cols)))
     model.add(Conv2D(32, kernel_size=(4,4), strides=(2,2), padding='same',
-                     kernel_initializer=initializers.RandomNormal(stddev=0.01),
+                     kernel_initializer=initializers.he_normal(),
                      activation='relu'))
     model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(256, activation='relu', kernel_initializer=he_normal()))
     model.add(Dense(num_actions, activation='linear')) 
 
     # plot the architecture of convnet 
     # plot_model(model, to_file='convnet.png')
 
     return model
-
-
-def get_output_folder(parent_dir, env_name, mode='train', experiment_id=0):
-    """Return save folder.
-
-    Assumes folders in the parent_dir have suffix -run{run
-    number}. Finds the highest run number and sets the output folder
-    to that number + 1. This is just convenient so that if you run the
-    same script multiple times tensorboard can plot all of the results
-    on the same plots with different names.
-
-    Parameters
-    ----------
-    parent_dir: str
-      Path of the directory containing all experiment runs.
-
-    Returns
-    -------
-    parent_dir/run_dir
-      Path to this run's save directory.
-    """
-    if not os.path.exists(parent_dir):
-        os.makedirs(parent_dir)
-
-    # find the latest experiment if not assigned
-    if experiment_id == 0:
-        for folder_name in os.listdir(parent_dir):
-            if not os.path.isdir(os.path.join(parent_dir, folder_name)):
-                continue
-            try:
-                folder_name = int(folder_name.split('-run')[-1])
-                if folder_name > experiment_id:
-                    experiment_id = folder_name
-            except:
-                pass
-
-    if mode == 'train':
-        experiment_id += 1
-
-    parent_dir = os.path.join(parent_dir, env_name)
-    parent_dir = parent_dir + '-run{}'.format(experiment_id)
-
-    if not os.path.exists(parent_dir):
-        os.makedirs(parent_dir)
-    return parent_dir
 
 
 def main(args):
@@ -241,14 +131,14 @@ def main(args):
     mem_size = 1000000
     window = 4
     input_shape = (84, 84)
-    if args.type == 'normal' or args.type == 'double':
+    if args.type in ['normal', 'double-DQN']:
         if args.netsize == 'small':
             model = create_model_small(window, input_shape, num_actions)
             target = create_model_small(window, input_shape, num_actions)
         elif args.netsize == 'large':
             model = create_model_large(window, input_shape, num_actions)
             target = create_model_large(window, input_shape, num_actions)
-    elif args.type == 'linear':
+    elif args.type in ['linear', 'double-Q']
         model = create_model_linear(window, input_shape, num_actions)
         target = create_model_linear(window, input_shape, num_actions)
     elif args.type == 'duel':
@@ -317,7 +207,7 @@ def parse_input():  # noqa: D103
     parser = argparse.ArgumentParser(description='Run DQN on Atari Space Invaders')
     parser.add_argument('--gpu', default=0, type=int, help='Atari env name')
     parser.add_argument('--env', default='SpaceInvaders-v0', help='Atari env name')
-    parser.add_argument('--type', default='normal', type=str, help='normal|double|duel|linear')
+    parser.add_argument('--type', default='normal', type=str, help='normal|double-DQN|duel|linear|double-Q')
     parser.add_argument('--simple', default=False, type=bool, help='True|False')
     parser.add_argument('--mode', default='train', help='train|test')
     parser.add_argument('--netsize', default='small', type=str, help='small|large')
@@ -328,7 +218,6 @@ def parse_input():  # noqa: D103
     parser.add_argument('--seed', default=0, type=int, help='Random seed')
 
     args = parser.parse_args()
-    # args.output = get_output_folder(args.output, args.env, args.mode, args.run)
     args.output = os.path.join(args.output, '%s-%s-%s'%(args.env, args.type, args.netsize))
     if not os.path.exists(args.output):
         os.makedirs(args.output)
